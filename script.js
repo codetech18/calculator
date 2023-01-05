09b27c10324936f622828ce48063d4dd3df457a0
codetech18 <akoredealao18@gmail.com>
2023-01-05 12:35:13 -0800

    updated ui and im working on the scientific functionality

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((item) => {
-    item.onclick = () => {
-        if (item.id == 'clear') {
+buttons.forEach((item) =>{
+    item.onclick = () =>{
+        if (item.id == 'clear'){
             display.innerText = '';
-        }else if (item.id == 'backspace') {
-            let string = display.innerText.toString();
-            display.innerText = string .substr(0, string.length - 1);
-        }else if (display.innerText != '' && item.id == 'equal') {
+        } else if (item.id == 'backspace') {
+            let String = display.innerText.tostring();
+            display.innerText = String.substr(0, string.length - 1);
+        } else if (display.innerText != '' && item.id == 'equal'){
             display.innerText = eval(display.innerText);
-        } else if (display.innerText == '' && item.id =='equal'){
-            display.innerText = 'Empty!';
-            setTimeout(() => (display.innerText = ''), 2000);
+        }  else if (display.innerText == '' && item.id =='equal'){
+            display.innerText = 'Empty';
+            setTimeout(() =>  (display.innerText =''), 2000);
+        } else if (item.id == 'sin') {
+            console.log('sin function called');
+            var input = document.getElementById("display").value;
+            if (isNaN(input)) {
+              display.innerText = 'Invalid input';
+            } else {
+              var radians = input * (Math.PI / 100);
+              var result = Math.sin(radians);
+              display.innerText = result;
+            }
+        } else if (item.id == 'cos') {
+            console.log('cos function called');
+            var input = document.getElementById("display").value;
+            if (isNaN(input)) {
+              display.innerText = 'Invalid input';
+            } else {
+              var radians = input * (Math.PI / 100);
+              var result = Math.cos(radians);
+              display.innerText = result;
+            }
+        } else if (item.id == 'sqrt2') {
+            console.log('squre function called');
+            var input = document.getElementById("display").value;
+            if (isNaN(input)) {
+              display.innerText = 'Invalid input';
+            } else {
+              var result = Math.sqrt(input);
+              display.innerText = result;
+            }
         } else {
-            display.innerText += item.id;
+            display.innerText += item.id
         }
     }
+    
 })
+
+// window.onload = function() {
+//     document.getElementById("sin").addEventListener("click", function sin() {
+//         console.log('sin function called');
+//         var input = document.getElementById("display").value;
+//         if (display.innerText === '') {
+//             display.innerText = 'Input required';
+//           } else {
+//             // input is present, check if it is a number
+//             var input = document.getElementById("display").value;
+//             if (isNaN(input)) {
+//               // input is not a number
+//               display.innerText = 'Invalid input';
+//             } else {
+//               // input is a number, calculate and display result
+//               var radians = input * (Math.PI / 100);
+//               var result = Math.sin(radians);
+//               display.innerText = result;
+//             }
+//           }
+          
+          
+    
+//     })
+// }
+
+
 
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
@@ -28,3 +85,7 @@ themeToggleBtn.onclick = () => {
     calculator.classList.toggle('dark');
     themeToggleBtn.classList.toggle('active');
 }
+
+
+
+
